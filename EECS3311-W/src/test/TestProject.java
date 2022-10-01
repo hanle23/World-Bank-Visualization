a44229dca03228bd821fd42fb0cb35faf48f3ce6
Han Le <hanle.cs23@gmail.com>
2022-10-01 12:57:10 -0400

    Added test functions

--- EECS3311-W/src/test/TestProject.java
+++ EECS3311-W/src/test/TestProject.java
@@ -1,5 +1,16 @@
 package test;
 
-public class TestProject {
+import org.junit.jupiter.api.Test;
+import src.*;
 
+public class TestProject {
+	@Test
+	public void testLogin(){
+		
+	}
+	
+	@Test
+	public void testDataFetcher() {
+		
+	}
 }
