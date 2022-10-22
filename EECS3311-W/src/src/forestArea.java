d410081b7a7629dd84ffb617d5a518cfa8ec095a
zohair-muhammad <77515434+zohair-muhammad@users.noreply.github.com>
2022-10-22 00:18:21 -0400

    Add files via upload

---
+++ EECS3311-W/src/src/forestArea.java
@@ -0,0 +1,47 @@
+package src;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class forestArea {
+	private dataFetcher jsonObject;
+	
+	public forestArea(int startYear, int endYear, String countryCode) {
+		if (isValid(startYear, endYear, countryCode)) {
+			this.jsonObject = new Adapter(startYear, endYear, countryCode);
+		}
+	}
+	
+	private boolean isValid(int startYear, int endYear, String countryCode) {
+		boolean result = true;
+		if (endYear < startYear) {
+			return false;
+		}
+		if (!util.COUNTRIES.contains(countryCode)) {
+			return false;
+		}
+		return result;
+	}
+	
+	public HashMap<Integer, Double> analyzeData() {
+		if (jsonObject == null) {
+			return null;
+		}
+		HashMap<Integer, Double> result = new HashMap<Integer, Double>();
+		HashMap<Integer, Double> forestArea = dataExtractor.filter(jsonObject.getData("AG.LND.FRST.ZS"));
+		for (Entry<Integer, Double> temp : forestArea.entrySet()) {
+			  Integer year = temp.getKey();
+			  Double forestPercent = temp.getValue();
+			  System.out.println("forest area percentage " + year + " is "+ forestPercent);
+			  System.out.println("percentage of the rest of the land used " + year + " is "+ (100-forestPercent));
+			  result.put(year, forestPercent)
+		return result;
+}
+		
+	public static void main(String args[]) {
+		forestArea test = new forestArea(2000, 2004, "can");
+		System.out.println(test.analyzeData());
+	}
+	
+
