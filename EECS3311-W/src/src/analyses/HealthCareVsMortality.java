4f5d895a48ca204c23f22f43c364a7dcca13f410
Jonah <big28@192.168.2.27>
2022-11-07 12:12:11 -0500

    Added HealthCareVsMortality (analysis #7 on handout)

---
+++ EECS3311-W/src/src/analyses/HealthCareVsMortality.java
@@ -0,0 +1,71 @@
+package src.analyses;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import src.dataExtractor;
+import src.util;
+import src.concrete.linkedList;
+import src.fetcher.Adapter;
+import src.fetcher.dataFetcher;
+import src.interfaces.Iterator;
+import src.interfaces.analyses;
+
+public class HealthCareVsMortality implements analyses{
+	private dataFetcher jsonObject;
+	public HealthCareVsMortality(int startYear, int endYear, String countryCode) {
+		if (isValid(startYear, endYear, countryCode)) {
+			this.jsonObject = new Adapter(startYear, endYear, countryCode);
+		}
+	}
+	
+	private boolean isValid(int startYear, int endYear, String countryCode) {
+		boolean result = true;
+		if (endYear < startYear) {
+			return false;
+		}
+		if (!util.COUNTRIES.contains(countryCode)) {
+			return false;
+		}
+		return result;
+	}
+	
+	public linkedList analyzeData() {
+		if (jsonObject == null) {
+			return null;
+		}
+		
+		linkedList dataSet1 = new linkedList();
+		linkedList dataSet2 = new linkedList();
+		HashMap<Integer, Double> problemsAccessingHealthCare = dataExtractor.filter(jsonObject.getData("SH.ACS.MONY.Q1.ZS"));
+		HashMap<Integer, Double> infantMortality = dataExtractor.filter(jsonObject.getData("SP.DYN.IMRT.IN"));
+		
+		for (Entry<Integer, Double> temp : problemsAccessingHealthCare.entrySet()) {
+			  Integer year = temp.getKey();
+			  Double healthCareAmount = temp.getValue();
+			  System.out.println("healthCareAmount for " + year + " is "+ healthCareAmount);
+			  Double infantMortalityAmount = infantMortality.get(year); 
+			  System.out.println("infantMortalityAmount for " + year + " is "+ infantMortalityAmount);
+			}
+		dataSet1.setdata(problemsAccessingHealthCare);
+		dataSet2.setdata(infantMortality);
+		dataSet1.setNext(dataSet2);
+		//result.setNext(null);
+		return dataSet1;
+	}
+	
+	public static void main(String args[]) {
+		HealthCareVsMortality test = new HealthCareVsMortality(2000, 2004, "can");
+		//System.out.println(test.analyzeData());
+		linkedList data = test.analyzeData();
+		while (data != null) {
+			Iterator dataIterator = data.getIterator();
+			HashMap<?,?> dataSet = data.getData();
+			for (Entry<?, ?> temp : dataSet.entrySet()) {
+				System.out.println("In result: co2/GDP for " + temp.getKey()+ " is " + temp.getValue());
+			}
+			data = (linkedList) dataIterator.next();
+			
+		}
+	}
+}
