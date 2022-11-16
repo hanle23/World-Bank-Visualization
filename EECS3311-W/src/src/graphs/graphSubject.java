ef9a549e5f37d525913cdbee921d5351b2378d56
Jonah <big28@LAPTOP-AKK0C43Q>
2022-11-16 14:07:47 -0500

    Added attach/detach methods to graphSubject

--- EECS3311-W/src/src/graphs/graphSubject.java
+++ EECS3311-W/src/src/graphs/graphSubject.java
@@ -1,6 +1,11 @@
 package src.graphs;
 
-import java.util.List;
+import java.io.FileInputStream;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Properties;
+import java.util.Vector;
+import java.util.Map.Entry;
 
 import javax.swing.JPanel;
 
@@ -8,26 +13,79 @@ import src.concrete.linkedList;
 import src.interfaces.Subject;
 
 public class graphSubject implements Subject{
-	private List<Graph> graphs;
+	private HashMap<String, Graph> graphs;
+	private static Properties props;
+	private GraphFactory factory;
 	
-	//Should we add attach/detach methods??
 	public graphSubject() {
-		//TODO
+		try {
+			props = new Properties();
+			props.load(new FileInputStream("src/src/config.properties"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Vector<String> graphNames = new Vector<String>();
+		graphNames.addAll(Arrays.asList(props.getProperty("Charts").split(",")));
+		graphs = new HashMap<String, Graph>();
+		factory = new GraphFactory();
+		
+		for(String graphType: graphNames) {
+			graphs.put(graphType, null);
+		}
 	}
 	
-	public List<Graph> getGraphs() {
+	public HashMap<String, Graph> getGraphs() {
 		return this.graphs;
 	}
 
-	public void setGraphs(List<Graph> graphs) {
+	public void setGraphs(HashMap<String, Graph> graphs) {
 		this.graphs = graphs;
 	}
 	
 	@Override
 	public void notifyGraphs(linkedList dataSets, JPanel west) {
-		for(Graph displayedGraph: this.graphs) {
-			displayedGraph.update(dataSets, west);
+		for(Entry<String, Graph> temp : this.graphs.entrySet()) {
+			Graph displayedGraph = temp.getValue();
+			
+			//only display the graph if the user selects it
+			if(displayedGraph != null) {
+				displayedGraph.update(dataSets, west);
+			}
 		}
+		
+	}
+
+	@Override
+	public void attach(String graphType) {
+		
+		//only adds/creates a graph if it wasn't previously added
+		if(graphs.get(graphType) == null) {
+			graphs.put(graphType, factory.getGraph(graphType));
+		}
+		System.out.println(graphs);
+		
+	}
+
+	@Override
+	public void detach(String graphType) {
+		
+		graphs.put(graphType, null);
+		System.out.println(graphs);
+		
+	}
+	
+	public static void main(String args[]) {
+		graphSubject test = new graphSubject();
+		test.attach("Pie Chart");
+		test.attach("Scatter Chart");
+		test.attach("Report");
+		test.attach("Scatter Chart");
+		test.detach("Pie Chart");
+		test.detach("Pie Chart");
+		test.detach("Bar Chart");
+		
 		
 	}
 
--- EECS3311-W/src/src/interfaces/Subject.java
+++ EECS3311-W/src/src/interfaces/Subject.java
@@ -5,6 +5,9 @@ import javax.swing.JPanel;
 import src.concrete.linkedList;
 
 public interface Subject {
-	//Should we add attach/detach methods??
+	
 	public void notifyGraphs(linkedList dataSets, JPanel west);
+	public void attach(String graphType);
+	public void detach(String graphType);
+	
 }
