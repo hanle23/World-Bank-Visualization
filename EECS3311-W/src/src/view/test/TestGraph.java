4250b5d7e272d5068a0ff0fd9e927fa30aa6db3a
zohair-muhammad <77515434+zohair-muhammad@users.noreply.github.com>
2022-11-30 22:29:06 -0500

    Add files via upload

--- EECS3311-W/src/src/view/test/TestGraph.java
+++ EECS3311-W/src/src/view/test/TestGraph.java
@@ -1,5 +1,43 @@
 package src.view.test;
+import org.junit.jupiter.api.Test;
+import src.view.concrete.*;
+import static org.junit.Assert.assertEquals;
+import src.view.graphs.pie;
+import java.util.HashMap;
+
 
 public class TestGraph {
-
+	@Test
+	public void testGraphFactory1(){
+		GraphFactory test = new GraphFactory();
+		
+		assertEquals(test.getGraph(null), null);
+	}
+	
+	@Test
+	public void testGraphFactory2(){
+		GraphFactory test = new GraphFactory();
+		assertEquals(test.getGraph("Pie Chart").getClass(), pie.class);
+	}
+	
+	@Test
+	public void testGraphSubject1() {
+		graphSubject test = new graphSubject();
+		assertEquals(test.getGraphs().size(), 6);
+	}
+	
+	@Test
+	public void testGraphSubject2() {
+		graphSubject test = new graphSubject();
+		test.attach("Pie Chart");
+		assertEquals(test.getGraphs().get("Pie Chart").getClass(), pie.class);
+	}
+	
+	@Test
+	public void testGraphSubject3() {
+		graphSubject test = new graphSubject();
+		test.attach("Pie Chart");
+		test.detachAll();
+		assertEquals(test.getGraphs().get("Pie Chart"), null);
+	}
 }
